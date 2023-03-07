0c4f7b161671136a7758a431ea87e0ea960604ff
Celso Jorge Sebastiao <celso.jorgesebastiao@unil.ch>
2023-03-07 17:31:26 +0100

    test création tableau

---
+++ exemple_objet.js
@@ -0,0 +1,34 @@
+let objet_musique_ = {
+    id : 1, 
+    emotion : "tristesse",
+    heure : "0900",
+    accompagne : true
+
+}
+let tableau_objets_musique = [
+    {
+        id : 1, 
+        emotion : "tristesse",
+        heure : "0900",
+        accompagne : true
+    },
+    {
+        id : 2, 
+        emotion : "joie",
+        accompagne : true
+    },
+    {
+        id : 3, 
+        emotion : ["joie","tristesse","dégoût"],
+        accompagne : true
+    },
+    {
+        id : 4, 
+        emotion : {
+            debut_du_morceau : "joie",
+            fin_du_morceau : "dégoût"
+        },
+        accompagne : true
+    }
+
+]
